d4c2cce6284afd25a3b41eb9c5dccd8a46e84f44
nicomjn <nicolasjeanmenne@gmail.com>
2021-12-01 15:15:33 +0100

    Add "Écart-type"

--- benchmark_tests/graph_interpret/task1-graph.py
+++ benchmark_tests/graph_interpret/task1-graph.py
@@ -14,16 +14,20 @@ philosophers_csv = pd.read_csv("time_results/csv/philosophers.csv")
 
 threads = np.unique([i for i in philosophers_csv["nThread"]])
 values = []
+std = []
 
 for thread in threads:
     sub_dataset = philosophers_csv.loc[philosophers_csv.nThread == thread]
     values.append(np.mean(sub_dataset["time"]))
+    std.append(np.std(sub_dataset["time"]))
 
-plt.plot(threads, values, color="blue", label="Valeur moyenne")
+plt.plot(threads, values, color="red", label="Valeur moyenne")
+plt.errorbar(threads, values, color="orange", label="Écart type", yerr=std, fmt='o', capsize=5)
 
 plt.xlabel("Nombre de threads")
 plt.ylabel("Temps d'exécution (en secondes)")
 
+plt.xlim(left=0)
 plt.ylim(bottom=0)
 
 plt.title("Temps d'exécution des threads pour le problème des philosophes")
@@ -48,16 +52,20 @@ producers_consumers_csv = pd.read_csv("time_results/csv/producers-consumers.csv"
 
 threads = [i for i in producers_consumers_csv["nThread"]]
 values = []
+std = []
 
 for thread in threads:
     sub_dataset = producers_consumers_csv.loc[producers_consumers_csv.nThread == thread]
     values.append(np.mean(sub_dataset["time"]))
+    std.append(np.std(sub_dataset["time"]))
 
-plt.plot(threads, values, color="blue", label="Valeur moyenne")
+plt.plot(threads, values, color="red", label="Valeur moyenne")
+plt.errorbar(threads, values, color="orange", label="Écart type", yerr=std, fmt='o', capsize=5)
 
 plt.xlabel("Nombre de threads")
 plt.ylabel("Temps d'exécution (en secondes)")
 
+plt.xlim(left=0)
 plt.ylim(bottom=0)
 
 plt.title("Temps d'exécution des threads pour le problème des producteurs consommateurs")
@@ -83,16 +91,20 @@ readers_writers_csv = pd.read_csv("time_results/csv/readers-writers.csv")
 
 threads = [i for i in readers_writers_csv["nThread"]]
 values = []
+std = []
 
 for thread in threads:
     sub_dataset = readers_writers_csv.loc[readers_writers_csv.nThread == thread]
     values.append(np.mean(sub_dataset["time"]))
+    std.append(np.std(sub_dataset["time"]))
 
-plt.plot(threads, values, color="blue", label="Valeur moyenne")
+plt.plot(threads, values, color="red", label="Valeur moyenne")
+plt.errorbar(threads, values, color="orange", label="Écart type", yerr=std, fmt='o', capsize=5)
 
 plt.xlabel("Nombre de threads")
 plt.ylabel("Temps d'exécution (en secondes)")
 
+plt.xlim(left=0)
 plt.ylim(bottom=0)
 
 plt.title("Temps d'exécution des threads pour le problème des lecteurs écrivans")
